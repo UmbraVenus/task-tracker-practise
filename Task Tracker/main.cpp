f04a81e627623f19cf2d51f41afec2ea254b133a
Umbra Venus <shirleyrsj@outlook.com>
2020-01-30 15:40:19 -0600

    Update main.cpp

--- Task Tracker/main.cpp
+++ Task Tracker/main.cpp
@@ -5,8 +5,239 @@
 //  Created by Sage Ren  on 1/28/20.
 //  Copyright © 2020 Sage Ren . All rights reserved.
 //
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <algorithm>
 
-#pragma warning(disable:4996);
+using namespace std;
+
+string choice1 = " 1. Add a task ";
+string choice2 = " 2. Add a goal ";
+string choice3 = " 3. Assign a task to a goal ";
+//string choice4 = "4. Sort task order";
+//string choice5 = "5. Delete task or goal";
+string choice6 = " 6. Exit the tracker ";
+
+vector<string> Tasks; //task order, and input
+vector<string> Goals; //goal input and ID
+
+void welcome();
+void txtToV(); // read only
+void printT(); // read only
+void printG();
+string mainMenu();
+string getDate(); //read only
+int parseUser(string);
+
+void welcome()
+{
+    cout << "======== Welcome to Task Tracker :) ======== " << endl;
+    string date = getDate();
+    cout << "==== It is " << date << " ====\n";
+}
+void txtToV()//generate vectors
+{
+    // Creating vectors from Parsing a txt file =========
+    fstream out;
+    out.open("tasks.txt");
+    string line;
+    vector<string> entries;
+    
+    while (getline(out, line)) //input each line into a vector
+    {
+        entries.push_back(line);
+    }
+    
+    string str;
+    vector<string>::iterator it; //declaring iterator
+    for (it = entries.begin(); it < entries.end(); it++)
+    {
+        str = *it;
+        char c = '@';
+        if (str.front() != c) //if 1st elem is @ -- goal
+        {
+            Tasks.push_back(str);
+        }
+        else
+        {
+            Goals.push_back(str);
+        }
+    }
+    out.close();
+    //Creating vectors from Parsing a txt file =========
+} //read only
+void printT()
+{
+    int i = 0;
+    fstream out("tasks.txt");
+    vector<int> taskSequence;
+    for (string elem : Tasks){
+        i++;
+        cout << " ( Task ) " << i << " [ " << elem << " ]" << endl;
+        taskSequence.push_back(i);
+    }
+    out.close();
+}
+void printG()
+{
+    fstream out("tasks.txt");
+    vector<int> goalSequence;
+    int j = 0;
+    for (string elem : Goals){
+        j++;
+        cout << " < Goal > " << j << " {{ " << elem << " }}" << endl;
+        goalSequence.push_back(j);
+    }
+    out.close();
+}
+string mainMenu()
+{
+    cout << "\n \\\\\\\\\\==== TASKS ====////////// \n" << endl;
+    // ========= print v to console ==========
+    printT();
+    printG();
+    // ======== print v to console ===========
+    cout << "\n //////////==== TASKS ====\\\\\\\\\\ \n" << endl;
+    
+    cout << "\n $$$ Menu options $$$ \n"; //menu options
+    cout << choice1 << choice2 << choice3 << choice6 << endl;
+    //cout << choice4 << endl;
+    //cout << choice5 << endl;
+    
+    string userSelection;
+    cout << "\n *** Enter 1-6 to Make Your Selection >> ";
+    getline(cin, userSelection);
+    //cin only gets the first component, use getline to get everything
+    
+    return userSelection;
+}
+string getDate()
+{
+    time_t now = time(0);
+    tm* ltm = localtime(&now);
+    
+    int year = 1900 + ltm->tm_year;
+    int month = 1 + ltm->tm_mon;
+    int day = ltm->tm_mday;
+    
+    string date = to_string(year) + to_string(month) + to_string(day);
+    
+    return date;
+}
+int parseUser(string input)
+{
+    getline(cin, input);
+    stringstream geek(input);
+    int x = 0;
+    geek >> x;
+    
+    return x;
+}
+
+int main()
+{
+    txtToV();
+    welcome();
+    string userSelection = mainMenu();
+    int selection = parseUser(userSelection);
+    
+    while ( selection )
+    {
+        switch ( selection )
+        {
+            case 1: // add new task
+            {
+                //FILE OPEN
+                fstream out("tasks.txt", ios::app); // open txt
+                string taskInput;
+                cout << "\n\n *** Please Enter New Task Here >> ";
+                getline(cin, taskInput);
+                string task = getDate() + " - " + taskInput; // format input into Tasks
+                Tasks.push_back(task);
+                out << task << endl; // to txt
+                cout << "\n ==== Added Task >> " << task << endl; // to console
+                //FILE CLOSE
+                out.close(); // close txt
+                break;
+            }
+            case 2: // add new goal
+            {
+                //FILE OPEN
+                fstream out("tasks.txt", ios::app); // open txt
+                string goalInput;
+                cout << "\n\n *** Please Enter New Goal Here >> ";
+                getline(cin, goalInput);
+                string goal = "@" + getDate() + " { " + goalInput + " } "; // format input to Goals
+                Goals.push_back(goal);
+                out << goal << endl; // to txt
+                cout << "\n ==== Added Goal >> { " << goal << " }" << endl; // to console
+                //FILE CLOSE
+                out.close(); // close txt
+                break;
+            }
+            case 3: //assign a goal to a task
+            {
+                string taskNumber;
+                string goalNumber;
+                
+                //get the task
+                cout << "\n\n ==== To assign a task to a goal";
+                cout << "\n ==== Please first select your task by entering the task number";
+                cout << "\n ==== Please then select the goal by entering the goal number" << endl;
+                printT();
+                cout << "\n *** Please First Enter Task Number Here >> ";
+                int taskN = parseUser(taskNumber); // transfer user answer to an int
+                string selectedT = Tasks[taskN];
+                
+                //get the goal to assign the task to
+                cout << "\n\n ==== What goal would you like to assign " << selectedT << " to?";
+                printG();
+                cout << "\n *** Please Enter the Goal Number Here >> ";
+                int goalN = parseUser(goalNumber);
+                string selectedG = Goals[goalN];
+                
+                //update the task
+                Tasks[taskN] = Tasks[taskN] + " { " + Goals[goalN] + " } ";
+                
+                // FILE OPEN
+                fstream out("tasks.txt", ios::trunc); // open the file to rewrite everything
+                
+                for (string elem : Tasks){ out << elem << endl; } // rewriting tasks
+                for (string elem : Goals){ out << elem << endl; } // rewriting goals
+                
+                cout << "\n\n ==== Congratulations! Your task has been updated! ==== " << endl;
+                cout << " ==== " << Tasks[taskN] << " ==== " << endl;
+                // FILE CLOSE
+                out.close();
+                break;
+            }
+            case 6: //exit command
+            {
+                cout << "\n ==== You have chosen: "<< choice6 << endl;
+                cout << "\n ==== Thank you so much for using the Task Tracker! ==== " << endl;
+                cout << "\n ==== Have a good one! :) ====\n" << endl;
+                exit(1);
+            }
+        }
+        mainMenu(); //looping
+    }
+    
+    printT();
+    printG();
+    cout << " !!!!!! BOOOM! !!!!!\n" << " !!!!! Your Tracker Exploded !!!!! \n";
+    
+    return 0;
+}
+
+/*#pragma warning(disable:4996);
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -108,7 +339,7 @@ int main() {
                 
                 break;
             }
-            /*case 4: //sort task order
+            case 4: //sort task order
             {
                 cout << "\nYou have chosen: "<< choice4 << endl;
             }
@@ -116,6 +347,7 @@ int main() {
             {
                 cout << "\nYou have chosen: "<< choice5 << endl;
             }*/
+/*
             case 6: //exit
             {
                 cout << "\n ==== You have chosen: "<< choice6 << endl;
@@ -230,3 +462,4 @@ void printMap(map<int,string> taskMap, map<string, int> goalMap)
     }
     out.close();
 }
+*/
